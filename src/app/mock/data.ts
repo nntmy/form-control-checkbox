8b9fe1f0bde608c1d96519fb9e6abc302e65fdbb
nntmy <kimanh24101997@gmail.com>
2019-11-03 12:30:57 +0700

    First commit

---
+++ src/app/mock/data.ts
@@ -0,0 +1,24 @@
+export const ARR = [
+    {
+        name: 'name1'
+    }, {
+        name: 'name2'
+    }, {
+        name: 'name3'
+    }, {
+        name: 'name4'
+    }
+];
+
+export const OBJ = {
+    destination: '0123',
+    array: [
+        {
+            name: 'name1'
+        }, {
+            name: 'name2'
+        }, {
+            name: 'name3'
+        }
+    ]
+};
